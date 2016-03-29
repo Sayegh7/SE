ec11b0237f86c6ca8d59419e3e32eaf92019180b
root <ahmedelsayegh7@gmail.com>
2016-03-29 05:12:42 -0700

    basic server

---
+++ server.js
@@ -0,0 +1,11 @@
+
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/public/' +  'index.html');
+});
+
+app.listen(3000);
